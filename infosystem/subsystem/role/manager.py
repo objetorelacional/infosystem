7b6981da42baec777fd83e4ca96018d9b26e56dc
Bruno Dantas <bdantas47@hotmail.com>
2020-07-02 18:00:56 +0000

    improve readability

--- infosystem/subsystem/role/manager.py
+++ infosystem/subsystem/role/manager.py
@@ -8,9 +8,8 @@ class CreatePolicies(operation.Operation):
         self.role_id = id
         self.application_id = application_id
 
-        return self.manager.api.applications.get(
-            id=application_id) is not None and self.driver.get(
-                id, session=session) is not None
+        return self.manager.api.applications.get(id=application_id) \
+            is not None and self.driver.get(id, session=session) is not None
 
     def _create_policy(self, role_id: str, capability_id: str) -> None:
         self.manager.api.policies.create(role_id=role_id,
