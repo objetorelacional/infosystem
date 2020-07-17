d5574cffbf05efc60fe2ff48a8b596b9fe83d0cf
Bruno Dantas <bdantas47@hotmail.com>
2020-07-17 18:53:49 +0000

    Update version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ REQUIRED_PACKAGES = [
 
 setup(
     name='infosystem',
-    version='0.2.9',
+    version='0.2.10',
     summary='Infosystem Framework',
     url='https://github.com/objetorelacional/infosystem',
     author='Samuel de Medeiros Queiroz, Francois Oliveira',
