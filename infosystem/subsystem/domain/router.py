6651d206f80d231c4d1f085c62450454d44db970
Bruno Dantas <bdantas47@hotmail.com>
2020-09-10 18:04:16 +0000

    Add routes for DomainSetting

--- infosystem/subsystem/domain/router.py
+++ infosystem/subsystem/domain/router.py
@@ -41,5 +41,26 @@ class Router(router.Router):
                 'url': self.resource_enum_url + '/activate/<id2>',
                 'callback': self.controller.activate,
                 'bypass': True
+            },
+            {
+                'action': 'Create a setting Domain',
+                'method': 'POST',
+                'url': self.resource_url + '/settings',
+                'callback': self.controller.create_setting,
+                'bypass': False
+            },
+            {
+                'action': 'Update a setting Domain',
+                'method': 'PUT',
+                'url': self.resource_enum_url + '/settings/<id2>',
+                'callback': self.controller.update_setting,
+                'bypass': False
+            },
+            {
+                'action': 'Remove a setting Domain',
+                'method': 'DELETE',
+                'url': self.resource_enum_url + '/settings/<id2>',
+                'callback': self.controller.remove_setting,
+                'bypass': False
             }
         ]
