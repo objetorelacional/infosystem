34c718edab3afa793eac6de300a3f1377ce5dd2e
Bruno Dantas <bdantas47@hotmail.com>
2020-10-29 14:44:40 -0300

    Add decorator to decide on run tasks
    Check configuration `USE_WORKER` and decide if run synchronous or asynchronous throught worker

--- infosystem/celery.py
+++ infosystem/celery.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask.globals import current_app
 from celery import Celery
 
 celery = Celery('infosystem')
@@ -15,3 +16,13 @@ def init_celery(app: Flask) -> Celery:
 
     celery.Task = ContextTask
     return celery
+
+
+def decide_on_run(fn):
+    def wrapper(*args):
+        should_use_worker = current_app.config['USE_WORKER']
+        if should_use_worker:
+            return fn.delay(*args)
+        else:
+            return fn(*args)
+    return wrapper
